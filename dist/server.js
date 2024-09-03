db6421aa6f8e85faeb394bc804b972b67aeb9aa9
Wesley Figueiredo <weslk1111@gmail.com>
2024-09-02 23:09:01 -0300

    atualização do dir do server.js

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('../app.js'); var _appjs2 = _interopRequireDefault(_appjs);
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
 
 const port = process.env.APP_PORT;
 
